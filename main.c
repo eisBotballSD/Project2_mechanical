e5c602c5ca3fa7fc679b195bcb08b0cdc449afa9
eisBotballSD <48925469+eisBotballSD@users.noreply.github.com>
2019-04-17 18:39:48 -0700

    Second pull request comment added

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ void move_backward_slowly(int mms);
 int main()
 {
     //Trying out a pull request - this didn't work the first time we did this
+    // still isn't working - I think we have to merge branches
     //wait_for_light(5);
     shut_down_in(120);
     
